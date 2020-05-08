ab60db1d7a6f104d9631d3b5050f3989942fee76
Mac <mac@MacBook-Air-de-Mac.local>
2020-05-08 13:30:36 -0300

    sumulando alteracao

--- app/src/main/java/br/com/erudio/Startup.java
+++ app/src/main/java/br/com/erudio/Startup.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
-
-
 import br.com.erudio.config.FileStorageConfig;
 
 @SpringBootApplication
